358ba7bffa430b6a635795a8b1023694c4a68474
Erlic Nguyen <contact.wilcity@gmail.com>
2026-03-23 15:02:01 +0700

    Done
    Signed-off-by: Erlic Nguyen <contact.wilcity@gmail.com>

--- prisma.config.sqlite.ts
+++ prisma.config.sqlite.ts
@@ -8,7 +8,6 @@ config(); // fallback to .env
 const sqliteUrl = process.env.DATABASE_URL_SQLITE ?? "file:./local.db";
 
 export default defineConfig({
-  earlyAccess: true,
   schema: path.join("prisma", "schema.sqlite.prisma"),
   datasource: {
     url: sqliteUrl,
--- prisma.config.ts
+++ prisma.config.ts
@@ -6,7 +6,6 @@ import { config } from "dotenv";
 config();
 
 export default defineConfig({
-  earlyAccess: true,
   schema: path.join("prisma", "schema.prisma"),
   datasource: {
     url: process.env.DIRECT_URL ?? process.env.DATABASE_URL!,
