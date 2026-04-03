c9892cb424fb929847cef75396f1ba59dec01303
Erlic Nguyen <contact.wilcity@gmail.com>
2026-04-03 09:35:13 +0700

    Done
    Signed-off-by: Erlic Nguyen <contact.wilcity@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "YT Payroll Platform",
   description: "Quản lý kênh YouTube và tính lương theo views",
+  verification: {
+    google: "6Va8pZjUeQRTVqmtXeSW2M6i1c921ab5tzK1pI9WTZo",
+  },
 };
 
 export default function RootLayout({
